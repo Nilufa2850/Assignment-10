de7e0c5e69322d2408324307c409dd272ccd9510
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-24 18:49:03 +0530

    Update Question-09.C

--- Question-09.C
+++ Question-09.C
@@ -1,1 +1,36 @@
+/*Write a function to check whether a given number contains a given digit or not.
+(TSRS) . */
+
+#include<stdio.h>
+int digit(int , int);
+int main()
+{
+    int number, dig;
+    printf("Enter a number : ");
+    scanf("%d", &number);
+    printf("Entet a digit : ");
+    scanf("%d", &dig);
+
+    printf("%d", digit(number, dig));
+
+    printf("\n");
+    return 0;
+}
+int digit(int num, int digit)
+{
+    int remi;
+    while(num)
+    {
+       remi = num % 10;
+       if (remi == digit)
+        return 1;
+       num = num / 10;
+    }
+
+    return 0; 
+        
+}
+
+
+
 
